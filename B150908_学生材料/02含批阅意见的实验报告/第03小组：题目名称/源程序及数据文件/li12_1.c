fe679792a473b97c7da406fbb5bfd28d5e7eee5b
B15090809 <2268018053@qq.com>
2016-04-28 00:20:52 +0800

    书店主函数

--- B150908_学生材料/02含批阅意见的实验报告/第03小组：题目名称/源程序及数据文件/li12_1.c
+++ B150908_学生材料/02含批阅意见的实验报告/第03小组：题目名称/源程序及数据文件/li12_1.c
@@ -2,219 +2,241 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include"file.h"
-#include"student.h"
+#include"book.h"
 
-void printHead( )      /*打印学生信息的表头*/
+void printHead( )      /*打印图书信息的表头*/
 {
-printf("%8s%10s%8s%6s%6s%8s%6s%6s\n","学号","姓名","性别","数学","英语","计算机","总分","名次");
+printf("%s\t %s\t %s\t %s\t %s\t %s\t %s\t %s\t %s\n","ISBN","书名","出版社","作者","售价","进价","销量","余量","利润");
 }
 
-void menu( )         /*顶层菜单函数*/
+void menu( )         	/*顶层菜单函数*/
 {
 		printf("******** 1. 显示基本信息 ********\n");
 		printf("******** 2. 基本信息管理 ********\n");
-		printf("******** 3. 学生成绩管理 ********\n");
-		printf("******** 4. 考试成绩统计 ********\n");
-     printf("******** 5. 根据条件查询 ********\n");
-		printf("******** 0. 退出         ********\n");
+		printf("******** 3. 图书销量管理 ********\n");
+		printf("******** 4. 图书利润统计 ********\n");
+		printf("******** 5. 根据条件查询 ********\n");
+		printf("******** 0. 退出       ********\n");
 }
 
-void menuBase( )     /*2、基本信息管理菜单函数*/
+void menuBase( )     /*基本信息管理菜单函数*/
 {
-		printf("%%%%%%%% 1. 插入学生记录 %%%%%%%%\n");
-		printf("%%%%%%%% 2. 删除学生记录 %%%%%%%%\n");
-		printf("%%%%%%%% 3. 修改学生记录 %%%%%%%%\n");
+		printf("%%%%%%%% 1. 插入图书记录 %%%%%%%%\n");
+		printf("%%%%%%%% 2. 删除图书记录 %%%%%%%%\n");
+		printf("%%%%%%%% 3. 查改图书记录 %%%%%%%%\n");
 		printf("%%%%%%%% 0. 返回上层菜单 %%%%%%%%\n");
 }
 
-void menuScore( )     /*3、学生成绩管理菜单函数*/
+void menuSale( )     /*图书销量管理菜单函数*/
 {
-		printf("@@@@@@@@ 1. 计算学生总分 @@@@@@@@\n");
-		printf("@@@@@@@@ 2. 根据总分排名 @@@@@@@@\n");
+		printf("@@@@@@@@ 1. 计算图书销量 @@@@@@@@\n");
+		printf("@@@@@@@@ 2. 根据销量排序 @@@@@@@@\n");
 		printf("@@@@@@@@ 0. 返回上层菜单 @@@@@@@@\n");
 }
  
-void menuCount( )    /*4、考试成绩统计菜单函数*/
+void menuProfit( )    /*图书利润统计菜单函数*/
 {
-		printf("&&&&&&&& 1. 求课程最高分 &&&&&&&&\n");
-		printf("&&&&&&&& 2. 求课程最低分 &&&&&&&&\n");
-		printf("&&&&&&&& 3. 求课程平均分 &&&&&&&&\n");
+		printf("&&&&&&&& 1. 求利润最大图书 &&&&&&&&\n");
+		printf("&&&&&&&& 2. 求利润最小图书 &&&&&&&&\n");
 		printf("&&&&&&&& 0. 返回上层菜单 &&&&&&&&\n");
 }
 
-void menuSearch()    /*5、根据条件查询菜单函数*/
+void menuSearch()    /*根据条件查询菜单函数*/
 {
-		printf("######## 1. 按学号查询   ########\n");
-		printf("######## 2. 按姓名查询   ########\n");
-		printf("######## 3. 按名次查询   ########\n");
+		printf("######## 1. 按ISBN查询   ########\n");
+		printf("######## 2. 按书名查询   ########\n");
+		printf("######## 3. 按作者查询   ########\n");
+		printf("######## 3. 按出版社查询   ########\n");
 		printf("######## 0. 返回上层菜单 ########\n");
 }
 
-int baseManage(Student stu[],int n)    	     /*该函数完成基本信息管理*/
-/*按学号进行插入删除修改，学号不能重复*/
+int baseManage(Book b[],int n)    	     /*该函数完成基本信息管理*/
 {  
-		int choice,t,find[NUM];
-     Student s;
-do
-	    {   
-menuBase( );                  /*显示对应的二级菜单*/
-printf("choose one operation you want to do:\n");
-		     scanf("%d",&choice);	          /*读入选项*/
-		     switch(choice)
-		     {
-			   case 1:	 readStu(&s,1);       /*读入一条待插入的学生记录*/
-					 n=insertStu(stu,n,s);   /*调用函数插入学生记录*/
-					 break;
-			   case 2:  printf("Input the number deleted\n");
-					 scanf("%ld",&s.num);  /*读入一个待删除的学生学号*/
-					 n=deleteStu(stu,n,s);   /*调用函数删除指定学号的学生记录*/
-					 break;
-			   case 3:  printf("Input the number modified\n");
-					 scanf("%ld",&s.num);  /*读入一个待修改的学生学号*/
-				      t=searchStu(stu,n,s,1,find) ; /*调用函数查找指定学号的学生记录*/
-				      if (t)                 /*如果该学号的记录存在*/
-					 {
-						  readStu(&s,1);   /*读入一条完整的学生记录信息*/
-					      stu[find[0]]=s;    /*将刚读入的记录赋值给需要修改的数组记录*/ 					 
-					  }					 
-					 else                 /*如果该学号的记录不存在*/ 
- printf("this student is not in,can not be modified.\n"); /*输出提示信息*/
-					 break;
-			    case 0: break;
+	int choice,t,find[NUM];
+	char judge1='n',judge2='n';
+	Book b1;
+	do
+	{   
+		menuBase( );					/*显示对应的二级菜单*/
+		printf("choose one operation you want to do:");
+		scanf("%d",&choice);	        /*读入选项*/
+		switch(choice)
+		{
+			case 1:	 readBook(&b1,1);   /*读入一条待插入的图书信息*/
+				n=insertBook(b,n,b1);   /*调用函数插入图书信息*/
+				break;
+			case 2:  printf("Input the number deleted\n");
+				scanf("%s",b1.ISBN);    	/*读入一个待删除图书的ISBN*/
+				n=deleteBook(b,n,b1);   	/*调用函数删除指定ISBN的图书记录*/
+				break;
+			case 3: do
+{					
+	printf("Input the ISBN you want search: \n");
+	scanf("%s",b1.ISBN);    /*读入一个待查找图书的ISBN*/
+	t=searchBook(b,n,b1,1,find) ; /*调用函数查找指定ISBN的图书记录*/
+	if (t)                  /*如果该ISBN的记录存在*/
+	{
+		printf("change this information? (y/n)\n");
+		scanf("%c",&judge2);
+		if (judge2 == 'y')   /*判断是否修改该条信息*/
+		{
+			readBook(&b1,1); /*读入一条完整的图书记录信息*/
+			b[find[0]]=b1;   /*将刚读入的记录赋值给需要修改的数组记录*/
+		}
+	}					 
+	else                     /*如果ISBN的记录不存在*/
+	{
+		printf("this book is not in.\ninput again? (y/n)\n"); /*输出提示信息*/
+		scanf("%c",&judge1);
+	}
+} while (judge1 == 'y');
+				break;
+			case 0: 
+                break;
 		    }
-	}while(choice);
-return n;                             /*返回当前操作结束后的实际记录条数*/
+    } while (choice);
+	return n;                               /*返回当前操作结束后的实际记录条数*/
 }
 
-void scoreManage(Student stu[],int n)          /*该函数完成学生成绩管理功能*/
+void saleManage(Book b[],int n)				/*该函数完成图书销量管理功能*/
 {  
 	int choice;
 	do
 	{
-		menuScore( );                        /*显示对应的二级菜单*/
-		printf("choose one operation you want to do:\n");
-		scanf("%d",&choice);	                 /*读入二级选项*/
+		menuSale( );                       	/*显示对应的二级菜单*/
+		printf("choose one operation you want to do:");
+		scanf("%d",&choice);                /*读入二级选项*/
 		switch(choice)
 		{
-			case 1:   calcuTotal(stu,n);         /*求所有学生的总分*/
-					  break;
-			case 2:   calcuRank(stu,n);         /*根据所有学生的总分排名次*/
-				      break;		
-			case 0:   break;
+			case 1: calcuTotal(b,n);        /*求所有图书的销量*/
+                printBook(b, n);
+				break;
+			case 2: sortBook(b,n,5);        /*根据图书销量排序*/
+                printBook(b, n);
+				break;
+			case 0: 
+				break;
 		}
 	}while(choice);
 }
 
-void printMarkCourse(char *s,double m[3][3],int k)   /*打印分数通用函数，被countManage 调用*/
-{                 /*形式参数k代表输出不同的内容，0、1、2分别对应最高分、最低分、平均分*/
-int i;
-    printf(s);                                  /*这里的s传入的是输出分数的提示信息*/
-    for (i=0;i<3;i++)                           /*i控制哪一门课*/
-		  printf("%10.2lf",m[i][k]);
-	   printf("\n");
+void profitManage(Book b[],int n)       /*该函数完成利润统计功能*/
+{
+    int choice;
+    char judge='n';                     /*定义judge默认为‘n’(no)*/
+    menuProfit( );                      /*显示对应的二级菜单*/
+    calcuProfit(b,n);					/*调用函数求各图书利润*/
+    sortBook(b, n, 3);
+    printBook(b,n);
+    printf("Do you need to reverse the form? (y\n)\n");
+    scanf("%c",&judge);
+    if (judge == 'y')
+    {
+        reverse(b,n);
+        printBook(b,n);
+    }
+    printf("choose one operation you want to do:\n");
+    scanf("%d",&choice);
 }
 
-void countManage(Student stu[],int n)               /*该函数完成考试成绩统计功能*/
+void searchManage(Book b[],int n)            /*该函数完成根据条件查询功能*/
 {
-		int choice;
-		double mark[3][3];
-		do
+    Book b1;
+	int i,choice,findnum,f[NUM];
+	do
+	{
+		menuSearch( );                       /*显示对应的二级菜单*/
+		printf("choose one operation you want to do:\n");
+		scanf("%d",&choice);
+		switch(choice)
 		{
-			menuCount( );                        /*显示对应的二级菜单*/
-			calcuMark(mark,stu,n);                 /*调用此函数求三门课的最高、最低、平均值*/
-			printf("choose one operation you want to do:\n");
-			scanf("%d",&choice);
-			switch(choice)
+			case 1:   printf("Input a book\'s ISBN will be searched:\n");
+			    scanf("%s",b->ISBN);         /*输入待查图书的ISBN*/
+				break;
+			case 2:   printf("Input a book\'s name will be searched:\n");
+				 scanf("%s",b->name);	     /*输入待查图书书名*/
+				 break;   
+			case 3:   printf("Input an author  will be searched:\n");
+				 scanf("%s",b->author);      /*输入待查图书的作者*/
+				 break;
+			case 4:   printf("Input a press  will be searched:\n");
+				  scanf("%s",b->press);      /*输入待查图书的出版社*/
+				  break;
+				case 0:   
+				  break;
+		}
+		if (choice>=1 && choice<=4)
+		{ 
+			findnum=searchBook(b,n,b1,choice,f);    /*查找的符合条件元素的下标存于f数组中*/
+			if (findnum)                            /*如果查找成功*/
 			{
-				case 1:   printMarkCourse("三门课的最高分分别是:\n",mark,0);  /*输出最高分*/
-				      break;
-				case 2:   printMarkCourse("三门课的最低分分别是:\n",mark,1);  /*输出最低分*/
-				      break;
-				case 3:   printMarkCourse("三门课的平均分分别是:\n",mark,2);  /*输出平均分*/
-				      break;
-				case 0:   break;
+				printHead( );                 /*打印表头*/
+				for (i=0;i<findnum;i++)       /*循环控制f数组的下标*/
+	      		printBook(&b[f[i]],1);        /*每次输出一条记录*/
 			}
-		}while (choice);
+		    else
+			    printf("this record does not exist!\n\n"); /*如果查找不到元素，则输出提示信息*/
+		 }
+	}while (choice);
 }
 
-void searchManage(Student stu[],int n)               /*该函数完成根据条件查询功能*/
-{
-    int i,choice,findnum,f[NUM];
-Student s;
-	   do
-{
-			menuSearch( );                         /*显示对应的二级菜单*/
-			printf("choose one operation you want to do:\n");
-			scanf("%d",&choice);
-			switch(choice)
-			{
-				case 1:   printf("Input a student\'s num will be searched:\n");
-				      scanf("%ld",&s.num);         /*输入待查询学生的学号*/
-					  break;
-				case 2:   printf("Input a student\'s name will be searched:\n");
-				      scanf("%s",s.name);	          /*输入待查询学生的姓名*/		  
-				      break;   
-				case 3:   printf("Input a rank will be searched:\n");
-				      scanf("%d",&s.rank);          /*输入待查询学生的名次*/
-					  break;
-				case 0:   break;
-			}
-		 	if (choice>=1&&choice<=3)
-			{ 
-				findnum=searchStu(stu,n,s,choice,f);    /*查找的符合条件元素的下标存于f数组中*/
-				if (findnum)				     /*如果查找成功*/
-				{
-			   		 printHead( );                  /*打印表头*/
-					 for (i=0;i<findnum;i++)         /*循环控制f数组的下标*/
-	      	 printStu(&stu[f[i]],1);      /*每次输出一条记录*/
-				}
-		    		else
-			    	    printf("this record does not exist!\n"); /*如果查找不到元素，则输出提示信息*/
-		     }		
-	    }while (choice);
-}
-
-int runMain(Student stu[],int n,int choice)    /*主控模块，对应于一级菜单其下各功能选择执行*/
+int runMain(Book b[],int n,int choice)        /*主控模块，对应于一级菜单其下各功能选择执行*/
 {
 		switch(choice)
 		{
-			case 1: printHead( );           /* 1. 显示基本信息*/
-				 sortStu(stu,n,1);         /*按学号由小到大的顺序排序记录*/ 
-          	 printStu(stu,n);          /*按学号由小到大的顺序输出所有记录*/
-					break;
-			case 2: n=baseManage(stu,n);    /* 2. 基本信息管理*/
+			case 1: printHead();              /* 1. 显示基本信息*/
+				 sortBook(b,n,1);             /*按ISBN由小到大的顺序排序记录*/
+				 printBook(b,n);              /*按ISBN由小到大的顺序输出所有记录*/
+				 break;
+			case 2: n=baseManage(b,n);        /* 2. 基本信息管理*/
 			   	     break;
-			case 3: scoreManage(stu,n);     /* 3. 学生成绩管理*/
-					break;
-			case 4: countManage(stu,n);     /* 4. 考试成绩统计*/
-					break;
-			case 5: searchManage(stu,n);     /* 5. 根据条件查询*/
-				     break;
-          case 0: break;
+			case 3: saleManage(b,n);          /* 3. 图书销量管理*/
+				break;
+			case 4: profitManage(b,n);        /* 4. 图书利润统计*/
+				break;
+			case 5: searchManage(b,n);        /* 5. 根据条件查询*/
+				break;
+			case 0: 
+				break;
 		}
 		return n;
 }
 
 int main( )
 {
-		Student stu[NUM];                /*定义实参一维数组存储学生记录*/
-      int choice,n;
-	 n=readFile(stu);                  /*首先读取文件，记录条数返回赋值给n*/
-	 if (!n)                          /*如果原来的文件为空*/
-	     {
-		    n=createFile(stu);              /*则首先要建立文件，从键盘上读入一系列记录存于文件*/
-}	 	 
-	do
-	     {
-	         menu();                      /*显示主菜单*/
-	         printf("Please input your choice: ");
-	         scanf("%d",&choice);
-	         if (choice>=0&&choice<=5)
-	              n=runMain(stu,n,choice);    /*通过调用此函数进行一级功能项的选择执行*/
-	         else 
-		          printf("error input,please input your choice again!\n");
+	 Book b[NUM];                 /*定义实参一维数组存储图书记录*/
+     int choice,n=0;              /*n赋值0*/
+	 printf("Input information from a file or keyboard? [1(file)/2(keyboard)]\n"); /*询问信息输入方式*/
+	 scanf("%d",&choice);         /*读入选择*/
+	 if (choice == 1)
+		n=readFile(b);            /*读取文件，记录条数返回赋值给n*/
+	 else if (choice == 2)
+	 {
+		 printf("\nInput an integer to indicate how many pieces of information to be input: \n");
+		 scanf("%d",&n);
+		 n=readBook(b,n);
+	 }
+	 else
+     {
+		 printf("choice error!\n");      /*输入非1、2数字按默认方式执行，从文件读取*/
+         printf("Execute by default：\n");
+     }
+	 if (!n)                     /*如果原来的文件为空*/
+	 {
+         printf("Target file is empty!\n");
+		 n=createFile(b);        /*则首先要建立文件，从键盘上读入一系列记录存于文件*/
+	 }	 	 
+	 do
+	 {
+	     menu();                 /*显示菜单*/
+	     printf("Please input your choice: ");
+	     scanf("%d",&choice);
+	     if (choice>=0 && choice<=5)
+			n=runMain(b,n,choice);    /*通过调用此函数进行一级功能项的选择执行*/
+	     else 
+		    printf("Error input,please input your choice again!\n");
 	} while (choice);
-	sortStu(stu,n,1);                   /*存入文件前按学号由小到大排序*/ 
-	     saveFile(stu,n);                   /*将结果存入文件*/
-      return 0;
+	sortBook(b,n,1);                  /*存入文件前按ISBN由小到大排序*/
+	saveFile(b,n);                    /*将结果存入文件*/
+    return 0;
 }
